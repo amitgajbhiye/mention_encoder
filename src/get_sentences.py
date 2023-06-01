72a1951664d836d052f4e04f541f92b2da20fceb
Amit Gajbhiye <amitgajbhiye03@gmail.com>
2023-06-01 12:09:52 +0100

    get cnetpchatgpt wiki sentences

--- src/get_sentences.py
+++ src/get_sentences.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pandas as pd
 
 sys.path.insert(0, os.getcwd())
 
@@ -45,14 +46,65 @@ def get_sentences(text_file, con_file, num_sents, outfile_name):
 
 inp_text_file = "/scratch/c.scmag3/en_wikipedia/en_wikipedia.txt"
 inp_concept = "dataset/cnetpchatgpt/con_vocab_cnetchatgpt.txt"
-out_file = "dataset/cnetpchatgpt/con_sentences.tsv"
 num_sents = 500
 
+
+############################
+out_file = "dataset/cnetpchatgpt/part2_con_sentences.tsv"
+
+
+all_con_file = "dataset/cnetpchatgpt/con_vocab_cnetchatgpt.txt"
+part1_file = "dataset/cnetpchatgpt/part1_con_sentences.tsv"
+
+part1_con = pd.read_csv(part1_file, sep="\t", names=["concept", "sent"])[
+    "concept"
+].unique()
+
+all_con = pd.read_csv(inp_concept, sep="\t", names=["concept"])["concept"].unique()
+
+part2_con = [con for con in all_con if con not in part1_con]
+part2_con.insert(0, "cooks")
+
+
+print(f"part1_con len : {len(part1_con)}")
+print(f"part2_con len : {len(part2_con)}")
+print(f"all_con len : {len(all_con)}")
+
+
+part2_inp_concept = "dataset/cnetpchatgpt/part2_con_vocab_cnetchatgpt.txt"
+with open(part2_inp_concept, "w") as outfile:
+    for con in part2_con:
+        outfile.write(f"{con.strip()}\n")
+
+############################
 
 if __name__ == "__main__":
     get_sentences(
         text_file=inp_text_file,
-        con_file=inp_concept,
+        con_file=part2_inp_concept,
         num_sents=num_sents,
         outfile_name=out_file,
+    )
+
+    parts = [
+        "dataset/cnetpchatgpt/part1_con_sentences.tsv",
+        "dataset/cnetpchatgpt/part2_con_sentences.tsv",
+    ]
+
+    df = pd.DataFrame()
+    for part in parts:
+        part_df = pd.read_csv(part, sep="\t", names=["concept", "sent"])
+        df = pd.concat((df, part_df), axis=0, ignore_index=True)
+
+    df.drop_duplicates(inplace=True)
+
+    final_file = "dataset/cnetpchatgpt/all_con_sentences.tsv"
+
+    print(f"final_df_shape : {df.shape}")
+
+    df.to_csv(
+        final_file,
+        sep="\t",
+        header=False,
+        index=False,
     )
